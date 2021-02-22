5689f21cf1eb89918938e66d5dd6660de7120276
Nate <37554478+servusDei2018@users.noreply.github.com>
2021-02-21 20:01:12 -0500

    Update alias.go

--- alias/alias.go
+++ alias/alias.go
@@ -10,7 +10,7 @@ type Alias struct {
 	Command string
 }
 
-// Return a new alias
+// NewAlias returns a new Alias.
 func NewAlias(name, cmd string) (*Alias, error) {
 	if len(name)<3{return &Alias{}, errors.New("alias: name is too short")}
 	if len(cmd)<1{return &Alias{}, errors.New("alias: command is too short")}
@@ -18,7 +18,7 @@ func NewAlias(name, cmd string) (*Alias, error) {
 	return &Alias{Name: name, Command: cmd}, nil
 }
 
-// Return a list of the alias' commands
+// Execute return a list of the alias' commands.
 func (a *Alias) Execute() []string {
 	return strings.Split(a.Command, ";")
 }
